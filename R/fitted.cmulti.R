9b36046c79ba0aaf88b05df29af7cf31a18f4ec2
Peter Solymos <solymos@ualberta.ca>
2018-01-31 04:33:40 +0000

    version 0.4-1

--- R/fitted.cmulti.R
+++ R/fitted.cmulti.R
@@ -1,8 +1,8 @@
 fitted.cmulti <-
 function(object, ...){
     type <- object$type
-    if (type == "mix")
+    if (type %in% c("mix", "fmix"))
         stop("fitted values are not available for finite mixture model")
-    X <- model.matrix(object$formula, object$model)
+    X <- model.matrix(object)
     poisson("log")$linkinv(drop(X %*% coef(object)))
 }
